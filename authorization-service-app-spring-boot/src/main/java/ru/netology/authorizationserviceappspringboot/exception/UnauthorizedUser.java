98feefc937ffa2d6805ffc1041972b2ece18fabd
Дмитрий Циркин <102895852+Dimka5555@users.noreply.github.com>
2022-12-03 19:09:35 +0300

    Delete UnauthorizedUser.java

--- authorization-service-app-spring-boot/src/main/java/ru/netology/authorizationserviceappspringboot/exception/UnauthorizedUser.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.authorizationserviceappspringboot.exception;
-
-public class UnauthorizedUser extends RuntimeException {
-    public UnauthorizedUser(String msg) {
-        super(msg);
-    }
-}
-
